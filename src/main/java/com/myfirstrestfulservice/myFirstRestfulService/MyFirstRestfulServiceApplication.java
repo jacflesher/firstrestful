99613ae4e579e60f796d0c83908db2e57360709e
Jac Flesher <jac.flesher@gmail.com>
2020-03-08 10:29:05 -0400

    adding oAuth2

--- src/main/java/com/myfirstrestfulservice/myFirstRestfulService/MyFirstRestfulServiceApplication.java
+++ src/main/java/com/myfirstrestfulservice/myFirstRestfulService/MyFirstRestfulServiceApplication.java
@@ -2,10 +2,12 @@ package com.myfirstrestfulservice.myFirstRestfulService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableAuthorizationServer
 
 public class MyFirstRestfulServiceApplication {
 
